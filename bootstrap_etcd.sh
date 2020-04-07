e3ce2477b93eb5a3b611ceccd5496b0366f2b1a9
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-04-08 00:37:42 +1000

    [bootstrap_etcd.sh] Switch mirror

--- bootstrap_etcd.sh
+++ bootstrap_etcd.sh
@@ -7,7 +7,7 @@ EXT="$2"
 ETCD_VERSION="${ETCD_VERSION-3.3.13}"
 DOWNLOAD_DIR="${DOWNLOAD_DIR-$HOME/Downloads}"
 INSTALL_DIR="${INSTALL_DIR-$HOME/bin/etcd}"
-curl -L 'https://github.com/etcd-io/etcd/releases/download/v'"$ETCD_VERSION"'/etcd-v'"$ETCD_VERSION"'-'"${TRIPLET}"'.'"${EXT}" -o "$DOWNLOAD_DIR"'/etcd.'"${EXT}"
+curl -L 'https://storage.googleapis.com/etcd/v'"$ETCD_VERSION"'/etcd-v'"$ETCD_VERSION"'-'"${TRIPLET}"'.'"${EXT}" -o "$DOWNLOAD_DIR"'/etcd.'"${EXT}"
 
 mkdir -p "$INSTALL_DIR"
 if [[ "$EXT" == "tar.gz" ]]; then
