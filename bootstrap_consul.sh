8ff94bd5d5c6e8d5a6328a931431323e556e14a6
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-04-08 01:06:19 +1000

    [bootstrap_*.sh] ARCHIVE introduction (TODO: rename extracted folders)

--- bootstrap_consul.sh
+++ bootstrap_consul.sh
@@ -7,10 +7,11 @@ EXT='zip'
 CONSUL_VERSION="${CONSUL_VERSION-1.5.1}"
 DOWNLOAD_DIR="${DOWNLOAD_DIR-$HOME/Downloads}"
 INSTALL_DIR="${INSTALL_DIR-$HOME/bin/consul}"
-curl -L 'https://releases.hashicorp.com/consul/'"$CONSUL_VERSION"'/consul_'"$CONSUL_VERSION"'_'"${TRIPLET}"'.'"${EXT}" -o "$DOWNLOAD_DIR"'/consul.'"${EXT}"
+ARCHIVE='consul_'"$CONSUL_VERSION"'_'"${TRIPLET}"'.'"${EXT}"
 
-mkdir -p "$INSTALL_DIR"
-unzip -q "$DOWNLOAD_DIR"'/consul.'"${EXT}" -d "$INSTALL_DIR"
+mkdir -p "$DOWNLOAD_DIR" "$INSTALL_DIR"
+curl -L 'https://releases.hashicorp.com/consul/'"$CONSUL_VERSION"'/'"$ARCHIVE" -o "$DOWNLOAD_DIR"'/'"$ARCHIVE"
+unzip -q "$ARCHIVE" -d "$INSTALL_DIR"
 
 pushd "$INSTALL_DIR"
 
--- bootstrap_zk.sh
+++ bootstrap_zk.sh
@@ -6,12 +6,15 @@ EXT='tar.gz'
 ZK_VERSION="${ZK_VERSION-3.5.5}"
 DOWNLOAD_DIR="${DOWNLOAD_DIR-$HOME/Downloads}"
 INSTALL_DIR="${INSTALL_DIR-$HOME/bin/zk}"
-curl -L 'https://archive.apache.org/dist/zookeeper/zookeeper-'"$ZK_VERSION"'/apache-zookeeper-'"$ZK_VERSION"'-bin.'"${EXT}" -o "$DOWNLOAD_DIR"'/zk.'"${EXT}"
+ARCHIVE='apache-zookeeper-'"$ZK_VERSION"'-bin.'"${EXT}"
 
-mkdir -p "$INSTALL_DIR"
-tar xzf "$DOWNLOAD_DIR"'/zk.'"${EXT}" -C "$INSTALL_DIR" --strip-components=1
+mkdir -p "$DOWNLOAD_DIR" "$INSTALL_DIR"
+curl -L 'https://archive.apache.org/dist/zookeeper/zookeeper-'"$ZK_VERSION"'/'"$ARCHIVE" -o "$DOWNLOAD_DIR"'/'"$ARCHIVE"
+tar xzf "$ARCHIVE" -C "$INSTALL_DIR" --strip-components=1
 
 pushd "$INSTALL_DIR"
+
+ls -alR "$HOME/zk"
 
 echo "tickTime=2000
 dataDir=$HOME/zk/data_
