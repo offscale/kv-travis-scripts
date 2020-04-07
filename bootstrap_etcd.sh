797c1e738bf227fcc4ea098e38a3d032df07a2cd
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-04-07 19:39:12 +1000

    [.gitignore] Init; [bootstrap_etcd.sh] Variable ??

--- bootstrap_etcd.sh
+++ bootstrap_etcd.sh
@@ -5,8 +5,8 @@ set -aeuo pipefail
 TRIPLET="$1"'-amd64'
 EXT="$2"
 ETCD_VERSION="${ETCD_VERSION-3.3.13}"
-DOWNLOAD_DIR="${DOWNLOAD_DIR:$HOME/Downloads}"
-INSTALL_DIR="${INSTALL_DIR:$HOME/bin/etcd}"
+DOWNLOAD_DIR="${DOWNLOAD_DIR-$HOME/Downloads}"
+INSTALL_DIR="${INSTALL_DIR-$HOME/bin/etcd}"
 curl -L 'https://github.com/etcd-io/etcd/releases/download/v'"$ETCD_VERSION"'/etcd-v'"$ETCD_VERSION"'-'"${TRIPLET}"'.'"${EXT}" -o "$DOWNLOAD_DIR"'/etcd.'"${EXT}"
 
 mkdir -p "$INSTALL_DIR"
