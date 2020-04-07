eb1326bddfba42e02aefa4f5eaa700ada3a7c72d
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-04-07 18:12:49 +1000

    [prepare_vcpkg.sh] "$BOOT_CODE"

--- prepare_vcpkg.sh
+++ prepare_vcpkg.sh
@@ -2,8 +2,8 @@
 
 set -aeuo pipefail
 
-DIR="$1"
-BOOT_CODE="$2"
+DIR=$1
+BOOT_CODE=$2
 
 if [[ -d "$DIR" ]] && [[ ! -d "$DIR"'/.git' ]]; then
 	rm -rf "$DIR"
@@ -12,8 +12,8 @@ fi
 if [[ ! -d "$DIR" ]]; then
 	mkdir -p $(dirname "$DIR")
 	git clone 'https://github.com/Microsoft/vcpkg' --depth=20 "$DIR"
-	cd "$DIR"
-	exit "$BOOT_CODE"
+	pushd "$DIR"
+	$BOOT_CODE
 else
 	cd "$DIR"
 	git reset --hard HEAD
