0fe05d373db99ac85b0f219a560fd9b69305b29e
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-04-09 22:10:14 +1000

    [bootstrap_consul.bash] Remove unused shared library import

--- bootstrap_consul.bash
+++ bootstrap_consul.bash
@@ -2,9 +2,6 @@
 
 set -aeuo pipefail
 
-declare -r DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-. "$DIR"'/shared.bash'
-
 declare -r TRIPLET="$1"'_amd64'
 declare -r EXT='zip'
 declare -r CONSUL_VERSION="${CONSUL_VERSION-1.5.1}"
