16c293f2cfe824c2e680e82217f546dbbac1fdb3
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-04-09 16:41:49 +1000

    [shared.bash] `pipefail` means boolean checks aren't needed; `shopt -u dotglob`

--- shared.bash
+++ shared.bash
@@ -6,11 +6,16 @@ set -aeuo pipefail
 unzip-strip() {
     local zip="$1"
     local dest="${2:-.}"
-    local temp="$(mktemp -d)" && unzip -d "$temp" "$zip" && mkdir -p "$dest" &&
-    shopt -s dotglob && local f=("$temp"/*) &&
+    local temp="$(mktemp -d)"
+    unzip -d "$temp" "$zip"
+    mkdir -p "$dest"
+    shopt -s dotglob
+    local f=("$temp"/*)
     if (( ${#f[@]} == 1 )) && [[ -d "${f[0]}" ]] ; then
         mv "$temp"/*/* "$dest"
     else
         mv "$temp"/* "$dest"
-    fi && rmdir "$temp"/* "$temp"
+    fi
+    rmdir "$temp"/* "$temp"
+    shopt -u dotglob
 }
